cf257d01f4450efebf1bb40614a9f116b0e71440
mortal_scode@163.com <mortal_scode@163.com>
2025-05-03 21:25:52 +0800

    feat: [LnWeb] implement and apply routing gourping

--- cmd/LnWeb/main.go
+++ cmd/LnWeb/main.go
@@ -10,19 +10,33 @@ func main() {
 	r.GET("/", func(c *LnWeb.Context) {
 		c.HTML(http.StatusOK, "<h1>Hello World</h1>")
 	})
-	r.GET("/hello", func(c *LnWeb.Context) {
-		// expect /hello?name=geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
 
-	r.GET("/hello/:name", func(c *LnWeb.Context) {
-		// expect /hello/geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *LnWeb.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello World</h1>")
+		})
 
-	r.GET("/assets/*filepath", func(c *LnWeb.Context) {
-		c.JSON(http.StatusOK, LnWeb.H{"filepath": c.Param("filepath")})
-	})
+		v1.GET("/hello", func(c *LnWeb.Context) {
+			// expect /hello?name=mortal
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *LnWeb.Context) {
+			// expect /hello/geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *LnWeb.Context) {
+			c.JSON(http.StatusOK, LnWeb.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+
+	}
 
 	r.Run(":8080")
 }
--- internal/service/LnWeb/gee.go
+++ internal/service/LnWeb/gee.go
@@ -1,6 +1,7 @@
 package LnWeb
 
 import (
+	"log"
 	"net/http"
 )
 
@@ -13,19 +14,22 @@ type Engine struct {
 	// 路由映射表：{ url : func }
 	//router map[string]HandlerFunc
 	router *router
+
+	*RouterGroup
+	groups []*RouterGroup // store all groups
 }
 
 // New : 构造一个 gee.Engine
 func New() *Engine {
-	return &Engine{
-		router: newRouter(),
-	}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
 func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	// 用以支持：GET-/hello 、 POST-/hello ...
-	//key := method + "-" + pattern
-	//engine.router[key] = handler
+	// key := method + "-" + pattern -> eg: GET-/hello 、 POST-/hello ...
+	// engine.router[key] = handler
 	engine.router.addRoute(method, pattern, handler)
 }
 
@@ -43,13 +47,50 @@ func (engine *Engine) Run(addr string) (err error) {
 
 // ServeHTTP 即对 Handler 接口的实现
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	//key := req.Method + "-" + req.URL.Path
-	//if handler, ok := engine.router[key]; ok {
-	//	handler(w, req)
-	//} else {
-	//	http.NotFound(w, req)
-	//}
-
 	c := newContext(w, req)
 	engine.router.handle(c)
+}
+
+/*
+	Engine ( core )
+	├── router: ( manage all routing groups )
+	└── RouterGroup ( default routing group )
+		├── prefix: ""
+		└── engine: ( point to Engine itself )
+*/
+
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc // support middleware
+	parent      *RouterGroup  // support nesting
+	engine      *Engine       // all groups share an Engine instance
+}
+
+// Group is defined to create a new RouterGroup
+// remember all groups share the same Engine instance
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) addRoute(method string, pattern string, handler HandlerFunc) {
+	pattern = group.prefix + pattern
+	log.Printf("Route %4s - %s\n", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
+}
+
+// GET defines the method to add GET request
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+
+// POST defines the method to add POST request
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 }
